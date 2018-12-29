cb7d03665617de7885c5c7b723398266d8ec2f6a
Ken Rettberg <kenrettberg@gmail.com>
2018-12-29 16:32:25 -0700

    fixed dumb ken mistake

--- app/controllers/intakes_controller.rb
+++ app/controllers/intakes_controller.rb
@@ -1,4 +1,16 @@
 class IntakesController < ApplicationController
-  def create
+  def index
+    "Get Yo' Drink On"
   end
+
+  def create
+    Intake.create(params)
+  end
+
+
+  # private
+
+  # def instake_params
+  #   params.require
+  # end
 end
