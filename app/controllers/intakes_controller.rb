a5c22c7ea7c23d6ed35ed60822e27e2cfe40751e
Ken Rettberg <kenrettberg@gmail.com>
2018-12-30 08:30:46 -0700

    more debugging

--- app/controllers/intakes_controller.rb
+++ app/controllers/intakes_controller.rb
@@ -1,11 +1,43 @@
 class IntakesController < ApplicationController
   def create
     p "*" * 100
-    p params
+    p params["payload"]
     p "*" * 100
-    Intake.create(user_id: params["payload"]["user"]["id"], flavor_id: params["payload"]["actions"][0]["value"])
+    Intake.create(intake_params)
+    # Intake.create(user_id: params["payload"]["user"]["id"], flavor_id: params["payload"]["actions"][0]["value"])
   end
 
+  private
+
+  def intake_params
+    p "*" * 200
+    p "user_id: #{params['payload']['user']['id']}"
+    p "flavor_id: #{params['payload']['actions'][0]['value']}"
+    p "*" * 200
+  end
+
+  # <ActionController::Parameters
+  #   {
+    #   "payload"=>
+    #   "{
+    #     "type":"interactive_message",
+    #     actions":[{"name":"pure",type":"button",value":"15"}],
+    #     callback_id":"lacroix",
+    #     team":{"id":"T04GF0BAF",domain":"beer-and-poop"},
+    #     channel":{"id":"CF4L9EQ8P",name":"la-croix-testin"},
+    #     user":{"id":"U04GF0BB5",name":"ken"},
+    #     action_ts":"1546137583.446588",
+    #     message_ts":"1546137577.012000",
+    #     attachment_id":"1",
+    #     token":"qV7XHOQeLC1g7e0VABcwXOId",
+    #     is_app_unfurl":false,
+    #     response_url":"https://hooks.slack.com/actions/T04GF0BAF/515078615590/4sMjegL07Sm8jWy2bv6bOCcR",
+    #     trigger_id":"513508642596.4559011355.8310d2c0d0a3ea78c7be44a31ad30185"}",
+    #     "controller"=>"intakes",
+    #     "action"=>"create"
+    #   }
+  #   permitted: false
+  # >
 
   # {
     # "payload"=>
@@ -26,11 +58,7 @@ class IntakesController < ApplicationController
       # }'
   # }
 
-  # private
 
-  # def instake_params
-  #   params.require('payload').permit('actions', 'user')
-  # end
 end
 
 
