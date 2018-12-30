02c62bf7fac77379d81151396c23e944fc74630e
Ken Rettberg <kenrettberg@gmail.com>
2018-12-29 18:31:18 -0700

    updated slack

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,23 +12,17 @@ class MessagesController < ApplicationController
         text: ':pear:',
         type: 'button',
         value: 2
-      },
+      }
     ]
-
-    attachments = [{
-      color: '#3AA3E3',
-      text: 'Choose a flava',
-      callback_id: 'lacroix',
-      actions: actions
-    }]
 
     args = {
       text: 'Record your La Croix intake, yo',
-      attachments: attachments
+      actions: actions,
+      action_text: 'Choose a flava'
     }
 
     slack_message = ::Slack.new(args)
-    slack_message.post_message
-    render status: 200
+    thing = slack_message.get_slack_args
+    render json: thing.to_json, status: 200
   end
 end
