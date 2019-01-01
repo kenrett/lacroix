c5a839b5aea16d4cec03729fdc97b8a65beff4d3
Ken Rettberg <kenrettberg@gmail.com>
2018-12-31 18:04:30 -0700

    added stats response when clicking button

--- app/controllers/intakes_controller.rb
+++ app/controllers/intakes_controller.rb
@@ -1,6 +1,10 @@
 class IntakesController < ApplicationController
   def create
-    Intake.create(user_id: intake_params[:user][:id], flavor_id: intake_params[:actions][0][:value])
+    Intake.create(
+      user_id: intake_params[:user][:id],
+      flavor_id: intake_params[:actions][0][:value]
+    )
+    show_stats
   end
 
   private
@@ -8,5 +12,29 @@ class IntakesController < ApplicationController
   def intake_params
     json_params = ActionController::Parameters.new(JSON.parse(params['payload']))
     json_params.permit(user: :id, actions: [:value])
+  end
+
+  def user_id
+    intake_params[:user][:id]
+  end
+
+  def flavor_id
+    intake_params[:actions][0][:value]
+  end
+
+  def get_total_count_for_past_seven_days
+    Intake.where(user_id: user_id).this_week
+  end
+
+  def total_today
+    Intake.where(user_id: user_id).today
+  end
+
+  def show_stats
+    flavor = Flavor.find(flavor_id).name
+    text = "#{flavor.titleize} recorded. You have had #{total_today.count} #{"LaCroix".pluralize(total_today.count)} today. #{total_today.by_flavor(flavor_id).count} of them have been #{flavor}."
+    slack_message = ::Slack.new(text: text)
+
+    render json: slack_message.to_json, status: 200
   end
 end
