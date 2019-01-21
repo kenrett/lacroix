ea49e10112c85d3091a8f98e5c56ac67c14789df
Ken Rettberg <kenrettberg@gmail.com>
2019-01-21 15:30:21 -0800

    return 3 flavors per line

--- app/lib/slack.rb
+++ app/lib/slack.rb
@@ -39,7 +39,7 @@ class Slack
     # format attachments
     if @actions.any?
       attachments = []
-      @actions.each_slice(1) do |a|
+      @actions.each_slice(3) do |a|
         attachments << {
           color: '#3AA3E3',
           text: "#{a[0][:name].titleize}",
