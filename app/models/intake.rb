6c1cd29e9a520dc34bd5d87284481a030794af78
Ken Rettberg <kenrettberg@gmail.com>
2019-01-21 16:08:32 -0800

    Fix today logic

--- app/models/intake.rb
+++ app/models/intake.rb
@@ -1,5 +1,5 @@
 class Intake < ApplicationRecord
   scope :this_week, -> { where('created_at >= ?', 1.week.ago) }
-  scope :today, -> { where('created_at > ?', 1.day.ago)}
+  scope :today, -> { where('created_at > ?',  Time.zone.now.beginning_of_day) }
   scope :by_flavor, ->(flavor) { where('flavor_id = ?', flavor) }
 end
